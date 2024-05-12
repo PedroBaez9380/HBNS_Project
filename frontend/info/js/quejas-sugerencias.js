76c52c625f1f1adfc1448f02e883696add10d653
Pedro <pedro_alberto_najera@hotmail.com>
2024-05-12 17:38:41 -0600

    funcionalidad de quejasSugerencias con api para conexion con base de datos La api sera agregada al directorio en el siguiente commit

---
+++ frontend/info/js/quejas-sugerencias.js
@@ -0,0 +1,44 @@
+function crearQuejaSugerencia() {
+    var formattedDate = new Date().toISOString().slice(0, 10);
+    $.ajax({
+        url: "https://localhost:7122/QuejasSugerencias/Guardar",
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        
+        data: JSON.stringify({
+            "nombre": $("#nombre").val(),
+            "correo": $("#email").val(),
+            "descripcion": $("#descripcion").val(),
+            "fecha": formattedDate
+        }),
+        crossDomain: true
+    }).done(function (result) {
+        alert("Gracias por tus comentarios!");
+        console.log(result);
+        $("#form-quejas-sugerencias")[0].reset();
+        
+    }).fail(function (xhr, status, error) {
+        alert(error)
+    });
+}
+
+$("#form-quejas-sugerencias").submit(function (event) {
+    if ($("#nombre").val() === "" || $("#email").val() === "" || $("#descripcion").val() === "") {
+        alert("Favor de completar los campos")
+        event.preventDefault()
+        event.stopPropagation()
+    } else {
+        if (!this.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        } else {
+            crearQuejaSugerencia();
+            event.preventDefault();
+            ("#form-quejas-sugerencias").classList.remove('was-validated')
+        }
+        this.classList.add('was-validated')
+    }
+    
+});
+
