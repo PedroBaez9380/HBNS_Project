e8804b86cc11f740b1e6fff54c47246e6ac35b1c
Pedro <pedro_alberto_najera@hotmail.com>
2024-05-19 11:43:30 -0600

    lineas borradas en quejas-sugerencias

--- backend/ApiHBNS/obj/Debug/net8.0/ApiHBNS.AssemblyInfo.cs
+++ backend/ApiHBNS/obj/Debug/net8.0/ApiHBNS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiHBNS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d423fe5fac96b1ed1a518ce0822196030626e18c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e236be3cb34706cf106e1ca31cba2a103c6d429")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiHBNS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiHBNS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
